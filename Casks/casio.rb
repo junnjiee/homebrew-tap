0b1ce13f9dbf23782685ae52e10eea3793a7bdbd
jj <71215065+junnjiee@users.noreply.github.com>
2026-05-02 00:52:26 +0800

    syntax err

--- Casks/casio.rb
+++ Casks/casio.rb
@@ -1,6 +1,6 @@
 cask "casio" do
   version "0.0.2"
-  sha256 "9620f35ec1c5a91703d153ff59e56255db8aef9f9fdd293e912fa748d4893395""
+  sha256 "9620f35ec1c5a91703d153ff59e56255db8aef9f9fdd293e912fa748d4893395"
 
   url "https://github.com/junnjiee/casio/releases/download/v#{version}/casio-#{version}.zip"
   name "Casio"
